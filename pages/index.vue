1c23782a0c529c1af40226743e638adfe0883139
Nawho <Nawho>
2023-05-02 17:43:35 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Leto</h1>
+</template>
---
+++ pages/text_summary.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Input your study material</h1>
+    <main>
+        <input type="text" v-model="studyText">
+        <button @click="sendRequest"> Submit</button>
+    </main>
+</template>
+
+<script setup lang="ts">
+const studyText = ref("")
+
+
+async function sendRequest() {
+    fetch 
+}
+</script>
